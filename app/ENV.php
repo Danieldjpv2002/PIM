7281a386c754f596e2727cdc4cc2e8e2ba9ebe4f
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2023-11-26 11:59:12 -0500

    NTS

--- app/ENV.php
+++ app/ENV.php
@@ -4,7 +4,7 @@ namespace App;
 
 class ENV
 {
-    const APP_CORRELATIVE = 'ticket';
+    const APP_CORRELATIVE = 'pim';
     const APP_NAME = 'PIM';
     const APP_LONGNAME = 'PIM by Pacheco';
     const APP_URL = 'https://pim.sode.me';
@@ -13,7 +13,7 @@ class ENV
     const DB_CONNECTION = 'mysql';
     const DB_PORT = '3306';
     const DB_SOCKET = '';
-    const DB_DATABASE = 'u163873840_ticket_db';
+    const DB_DATABASE = 'u163873840_pim_db';
 
     # LOCAL
     // const DB_HOST = 'localhost';
@@ -22,7 +22,7 @@ class ENV
 
     # PRODUCCION
     const DB_HOST = '154.56.47.1';
-    const DB_USERNAME = 'u163873840_ticket_user';
+    const DB_USERNAME = 'u163873840_pim_user';
     const DB_PASSWORD = 'HLy9^OhLt{-73v3wogghIl£S-n!nu\0_FC}T';
 
     const MAIL_MAILER = 'smtp';
